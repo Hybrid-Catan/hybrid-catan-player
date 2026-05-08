27232ac8c6d266076edc444b6da3d658dbbc4f63
evanng2610 <evanng2610@gmail.com>
2026-05-09 00:13:21 +1000

    createGame.ts had a minor code layout update to match with getGame.ts structure. No functionality changed

--- lib/api/game/createGame.ts
+++ lib/api/game/createGame.ts
@@ -3,12 +3,8 @@ const BASE_URL = "http://localhost:3000";
 export async function createGame(gameId: string) {
   const res = await fetch(`${BASE_URL}/api/init/gamestate`, {
     method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      gameId,
-    }),
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ gameId }),
   });
 
   return res.json();
